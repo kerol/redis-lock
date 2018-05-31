2e968129ca510b6a640bcf6753821e39456067d8
kerol <kerol@kepiture.com>
2018-05-31 17:27:37 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="redlock-dist",
-    version="1.0.0",
+    version="1.0.1",
     author="kerol",
     author_email="ikerol@163.com",
     description="Distributed locks with Redis",
